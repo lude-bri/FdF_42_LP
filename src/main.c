02cc70e6cb091b46ed3111abc496d010573f1841
Luigi De Brito Piantavinha <szochoralewizc@hotmail.com>
2024-09-25 10:57:34 +0100

    libft_comments_mem

--- src/main.c
+++ src/main.c
@@ -16,19 +16,44 @@
 #define W 800
 #define H 500
 
-int	main(void)
+// int	main(void)
+// {
+// 	void	*mlx;
+// 	void	*mlx_window;
+//
+// 	mlx = mlx_init();
+// 	mlx_window = mlx_new_window(mlx, W, H, "FdF");
+// 	for (int y = H * 0.1; y < H * 0.9; y++)
+// 	{
+// 		for (int x = W * 0.1; x < W * 0.9; x++)
+// 		{
+// 			mlx_pixel_put(mlx, mlx_window, x, y, rand() % 0x100000);
+// 		}
+// 	}
+// 	mlx_loop(mlx);
+// }
+
+
+typedef struct	s_vars
 {
 	void	*mlx;
-	void	*mlx_window;
+	void	*win;
+}			t_vars;
 
-	mlx = mlx_init();
-	mlx_window = mlx_new_window(mlx, W, H, "FdF");
-	for (int y = H * 0.1; y < H * 0.9; y++)
-	{
-		for (int x = W * 0.1; x < W * 0.9; x++)
-		{
-			mlx_pixel_put(mlx, mlx_window, x, y, rand() % 0x100000);
-		}
-	}
-	mlx_loop(mlx);
+static int	close(t_vars *vars)
+{
+	mlx_destroy_window(vars->mlx, vars->win);
+	return (0);
 }
+
+int	main(void)
+{
+	t_vars	vars;
+
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, W, H, "Test");
+	mlx_hook(vars.win, 2, 1L<<0, close, &vars);
+	mlx_loop(vars.mlx);
+}
+
+
