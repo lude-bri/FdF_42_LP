f1f681b4d2117ffe866fa8150d6a2b71dc1584b8
Luigi De Brito Piantavinha <szochoralewizc@hotmail.com>
2024-09-26 11:15:19 +0100

    FDF_Header

--- inc/fdf.h
+++ inc/fdf.h
@@ -6,7 +6,7 @@
 /*   By: luigi <luigi@student.42porto.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 22:49:27 by luigi             #+#    #+#             */
-/*   Updated: 2024/09/23 23:42:41 by luigi            ###   ########.fr       */
+/*   Updated: 2024/09/26 11:15:00 by luigi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,14 @@
 # define FDF_H
 
 # include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <time.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <X11/X.h>
+# include <X11/keysym.h>
+# include <math.h>
 # include "../lib/mlx/mlx.h"
 
 //This holds the values of watch point on the map
